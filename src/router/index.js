dbde4c921377ccc3840e7653a2e3c6f572a16915
Urich Oliveira <oliveira.urich@gmail.com>
2021-01-11 19:49:55 -0300

    Limpeza de arquivos

--- src/router/index.js
+++ src/router/index.js
@@ -21,15 +21,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "register" */ "../views/account/Register.vue")
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
